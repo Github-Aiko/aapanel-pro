b845945668c021a9de5d528a270ada26d8ef16cf
Aiko <aikocute@icloud.com>
2023-04-24 17:27:33 +0700

    Update aapro.sh

--- aapro.sh
+++ aapro.sh
@@ -5,18 +5,18 @@ chattr +i /www/server/panel/data/plugin.json
 chattr -i /www/server/panel/data/repair.json
 rm /www/server/panel/data/repair.json
 cd /www/server/panel/data
-wget https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/resource/repair.json
+wget https://raw.githubusercontent.com/Github-Aiko/aapanel-pro/master/resource/repair.json
 chattr +i /www/server/panel/data/repair.json
 rm /www/server/panel/data/plugin.json
 cd /www/server/panel/data
-wget https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/resource/plugin.json
+wget https://raw.githubusercontent.com/Github-Aiko/aapanel-pro/master/resource/plugin.json
 chattr +i /www/server/panel/data/plugin.json
 rm /www/server/panel/BTPanel/static/js/soft.js
 cd /www/server/panel/BTPanel/static/js
-wget https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/resource/soft.js
+wget https://raw.githubusercontent.com/Github-Aiko/aapanel-pro/master/resource/soft.js
 chattr +i /www/server/panel/data/soft.js
 rm /www/server/panel/BTPanel/static/js/index.js
 cd /www/server/panel/BTPanel/static/js
-wget https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/resource/index.js
+wget https://raw.githubusercontent.com/Github-Aiko/aapanel-pro/master/resource/index.js
 chattr +i /www/server/panel/data/index.js
 red "Done"
